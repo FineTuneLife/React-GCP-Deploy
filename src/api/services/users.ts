7e4b38460fb2077e55ce14708a839172a2eed77a
DDiscua <ddiscua.dragon@gmail.com>
2023-07-13 13:31:44 -0600

    Init pipe setup

---
+++ src/api/services/users.ts
@@ -0,0 +1,21 @@
+import { axiosClient } from "../axiosClient";
+import { User } from "../../models";
+
+const getUsers = async (): Promise<User[]> => {
+    try {
+        const response = await axiosClient.get('/users');
+        await delay(1000);
+
+        const result: User[] = response.data;
+        return result;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+
+}
+export { getUsers };
+
+const delay = (ms: number) => new Promise(
+    resolve => setTimeout(resolve, ms)
+);
